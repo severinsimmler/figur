1a3ff366444ec50b76d094c7b917fb0ce0eaf090
severinsimmler <severin.simmler@posteo.de>
2019-02-27 19:14:56 +0100

    feat: add test

---
+++ tests/test_figur.py
@@ -0,0 +1,13 @@
+import pytest
+import figur
+
+
+TEXT = "Der Gärtner entfernte sich eilig, und Eduard folgte bald."
+
+def test_tagging():
+    tagged = figur.tag(text)
+    counts = tagged["Tags"].value_counts()
+    assert counts["AppTdfW"] == 1
+    assert counts["pron"] == 1
+    assert counts["Core"] == 1
+    assert counts["_"] == 8
