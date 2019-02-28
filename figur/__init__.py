2ecc2204cd0d18e019b2ce5a1d7f359426858fb6
severinsimmler <severin.simmler@posteo.de>
2019-02-28 14:51:33 +0100

    fix: example

--- figur/__init__.py
+++ figur/__init__.py
@@ -8,18 +8,16 @@ Figurenerkennung for German literary texts
 >>> import figur
 >>> text = "Der Gärtner entfernte sich eilig, und Eduard folgte bald."
 >>> figur.tag(text)
-    SentenceId      Token      Tag
-0            0        Der        _
-1            0    Gärtner  AppTdfW
-2            0  entfernte        _
-3            0       sich     pron
-4            0      eilig        _
-5            0          ,        _
-6            0        und        _
-7            0     Eduard     Core
-8            0     folgte        _
-9            0       bald        _
-10           0          .        _
+   SentenceId      Token      Tag
+0           0        Der        _
+1           0    Gärtner  AppTdfW
+2           0  entfernte        _
+3           0       sich     pron
+4           0     eilig,        _
+5           0        und        _
+6           0     Eduard     Core
+7           0     folgte        _
+8           0      bald.        _
 ```
 """
 
